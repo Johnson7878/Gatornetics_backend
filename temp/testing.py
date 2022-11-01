1321fce97fe61e16cda477bf941f506e3331208c
Cole McEwen Johnson <65368500+Johnson7878@users.noreply.github.com>
2022-11-01 16:52:42 -0400

    Completed Pre-Alpha Backend
    In this backend, we have compiled EVERYTHING for the pre-alpha build. There is a machine learning example, and code that represents how we would generate csv's that would then be configured into MySQL. Additionally, we gather data from the CFBD API.

--- temp/testing.py
+++ temp/testing.py
@@ -6,6 +6,9 @@ from cfbd.rest import ApiException
 from pprint import pprint
 import numpy as np
 import csv
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+plt.style.use('bmh')
 
 # Configure API key authorization: ApiKeyAuth
 configuration = cfbd.Configuration()
@@ -83,3 +86,53 @@ with open('playerInfo.csv','w', encoding='UTF8', newline='') as f1:
     writer2 = csv.writer(f1)
     writer2.writerow(header2)
     writer2.writerows(SEC_rosters)
+
+
+#now lets create a ML starter that will shwocase a common example
+#Here, we'll load in a players season stats (Anthony Richardson, UF) and plot them on a graph
+#Then, we'll take the player's data and put it into a matrix (pre-process) and send it through Linear regression model
+#from ScikitLearn.
+
+# For the example below, we'll focus on total passing yards
+api_instance2 = cfbd.PlayersApi(cfbd.ApiClient(configuration))
+year = 2022 # int | Year filter
+team = 'Florida' # str | Team filter (optional)
+conference = 'SEC' # str | Conference abbreviation filter (optional)
+start_week = 1 # int | Start week filter (optional)
+end_week = 9 # int | Start week filter (optional)
+season_type = 'regular' # str | Season type filter (regular, postseason, or both) (optional)
+category = 'passing' # str | Stat category filter (e.g. passing) (optional)
+
+
+totalyards = []
+def getvalz():
+    api_response = api_instance2.get_player_season_stats(year, team=team, conference=conference, start_week=start_week, end_week=end_week, season_type=season_type, category = category)
+    return api_response
+
+try:
+    # Player stats by season
+    for ___ in range(end_week,0,-1):
+        temp = getvalz()
+        for iter1 in temp:
+            
+            if iter1.player == 'Anthony Richardson':
+                if iter1.stat_type == 'YDS':
+                    totalyards += [iter1.stat]
+        end_week -= 1
+except ApiException as e:
+    print("Exception when calling PlayersApi->get_player_season_stats: %s\n" % e)
+
+totalyards.sort()
+weeks = np.array(list(range(1,10)))
+weeks = weeks.reshape(len(weeks),1)
+
+
+reg = LinearRegression().fit(weeks, totalyards)
+yards_pred = reg.predict(weeks)
+
+plt.plot(weeks, totalyards, 'o')
+plt.plot(weeks, yards_pred, 'orange')
+plt.xlabel('Weeks',size=15)
+plt.ylabel('Total Yards',size=15)
+plt.title('Total Passing Yards for AR15')
+plt.show()
